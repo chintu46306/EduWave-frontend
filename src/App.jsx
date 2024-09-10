2b7d662bafbd9b259f6a106482675c99bccf124b
chintu46306 <crg46306@gmail.com>
2024-09-11 00:59:55 +0530

    added:Signup,Login;Logout funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import HomeLayout from './Layouts/HomeLayout.jsx';
 import HomePage from './Pages/HomePage.jsx';
 import AboutUs from './Pages/AboutUs.jsx';
 import NotFound from './Pages/NotFound.jsx';
+import Signup from './Pages/Signup.jsx';
+import Login from './Pages/Login.jsx';
+
 
 
 function App() {
@@ -15,6 +18,11 @@ function App() {
       <Routes>
          <Route path="/" element={<HomePage />}></Route>  
          <Route path="/about" element={<AboutUs />}></Route> 
+
+
+         <Route path="/signup" element={<Signup />}></Route> 
+         <Route path="/login" element={<Login />}></Route> 
+
          <Route path="*" element={<NotFound />}></Route> 
 
 
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,15 +1,82 @@
-import { createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
+import axiosInstance from "../../Helpers/axiosInstance"
 const initialState = {
     isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
     data: localStorage.getItem('data') || {}
 };
 
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+    try {
+        const res = axiosInstance.post("user/register", data);
+        toast.promise(res, {
+            loading: "Wait! creating your account",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to create account"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+})
+
+export const login = createAsyncThunk("/auth/login", async (data) => {
+    try {
+        const res = axiosInstance.post("user/login", data);
+        toast.promise(res, {
+            loading: "Wait! authentication in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to log in"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+});
+
+export const logout = createAsyncThunk("/auth/logout", async () => {
+    try {
+        const res = axiosInstance.post("user/logout");
+        toast.promise(res, {
+            loading: "Wait! logout in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to log out"
+        });
+        return (await res).data;
+    } catch(error) {
+        toast.error(error?.response?.data?.message);
+    }
+})
+
 const authSlice = createSlice({
-    name : 'auth',
+    name: 'auth',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(login.fulfilled, (state, action) => {
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.user?.role
+        })
+        .addCase(logout.fulfilled, (state) => {
+            localStorage.clear();
+            state.data = {};
+            state.isLoggedIn = false;
+            state.role = "";
+        })
+    }
 });
 
 // export const {} = authSlice.actions;
