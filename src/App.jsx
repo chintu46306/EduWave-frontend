11244dfc68c4b2e3dedf1d4918ca3efdd367511c
chintu46306 <crg46306@gmail.com>
2024-09-26 17:08:06 +0530

    added: Denied page and Course Description

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
 import Contact from './Pages/Contact';
+import CourseDescription from './Pages/Course/CourseDescription';
 import CourseList from './Pages/Course/CourseList';
+import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
@@ -17,7 +19,10 @@ function App() {
         <Route path="/" element={<HomePage />} ></Route>
         <Route path="/about" element={<AboutUs />} ></Route>\
         <Route path="/courses" element={<CourseList />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
+
+        <Route path="/course/description" element={<CourseDescription />} />
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
