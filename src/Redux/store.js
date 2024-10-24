16aefcd45daf4afcbb3d0f38021a88aba614cb51
chintu46306 <crg46306@gmail.com>
2024-10-24 18:18:23 +0530

    added lecture state

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,12 +2,35 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authSliceReducer from './Slices/AuthSlice';
 import courseSliceReducer from './Slices/CourseSlice';
+import lectureSliceReducer from './Slices/LectureSlice';
+//import razorpaySliceReducer from './Slices/RazorpaySlice';
+
+
 const store = configureStore({
     reducer: {
         auth: authSliceReducer,
-        course: courseSliceReducer
+        course: courseSliceReducer,
+       // razorpay: razorpaySliceReducer,
+        lecture: lectureSliceReducer
     },
     devTools: true
 });
 
 export default store;
+
+
+
+
+// import { configureStore } from "@reduxjs/toolkit";
+
+// import authSliceReducer from './Slices/AuthSlice';
+// import courseSliceReducer from './Slices/CourseSlice';
+// const store = configureStore({
+//     reducer: {
+//         auth: authSliceReducer,
+//         course: courseSliceReducer
+//     },
+//     devTools: true
+// });
+
+// export default store;
