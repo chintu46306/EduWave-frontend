641130bd232d7b378918d8d67f82dd838833d21e
chintu46306 <crg46306@gmail.com>
2024-12-17 14:20:16 +0530

    fix: tailwinf config error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+import lineClamp from "@tailwindcss/line-clamp"
+
 export default {
   content: ["./index.html","./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
+  plugins: [daisyui, lineClamp],
   daisyui: { themes: ["dark"], }
 };
 
